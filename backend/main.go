79303eb96bb77a773342fc00bdc9a9a99a30e0a4
zehan khan <zehan9211@gmail.com>
2024-06-01 02:16:04 +0530

    feat: update main

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,24 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
+	// creating app
 	app := fiber.New()
 
+	// group routes to v1
+	apiv1 := app.Group("/api/v1")
+
+	// routes
 	app.Get("/", handleHome)
+	apiv1.Get("/", handleIndex)
+
+	// server listing
 	app.Listen(":3000")
 }
 
 func handleHome(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "server is running!"})
+}
+
+func handleIndex(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "welcome to hotel reservation index!"})
 }
