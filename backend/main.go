f45e9e974070847bf7fe87d0bd8137d4f87491ce
zehan khan <zehan9211@gmail.com>
2024-06-02 01:18:41 +0530

    feat: install mongodb and setup mongo connection

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/zehan12/hotel-reservation/backend/api"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	dbUri  = "mongodb://localhost:27017"
+	dbName = "hotel-reservation"
 )
 
 func main() {
+
+	// database setup
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Fatalf("failed to connect with db: %v", err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	// verify mongoDB connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("failed to ping MongoDB: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	client.Database(dbName)
+
 	// creating app
 	app := fiber.New()
 
--- backend/types/user.go
+++ backend/types/user.go
@@ -1,7 +1,7 @@
 package types
 
 type User struct {
-	Id        string `bson:"_id" json:"id"`
+	Id        string `bson:"_id,omitempty" json:"id,omitempty"`
 	FirstName string `bson:"firstName" json:"firstName"`
 	LastName  string `bson:"lastName" json:"lastName"`
 }
